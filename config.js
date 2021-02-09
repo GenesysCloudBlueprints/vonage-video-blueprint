5c5e149ce625ca401c1dbbcf852fed89cad55bdd
hero2bash <zero.bash@gmail.com>
2021-02-09 16:58:30 +0800

    Modify config

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 module.exports = {
     expressPort: 443,
     testMode: false, 
-    // testMode: false,
 
     vonage: {
-        apiKey: '46811084',
-        apiSecret: 'a539412a404b528c60ae64b87b617d33b0e24fcd'
+        apiKey: '',
+        apiSecret: ''
     },
 
     genesysCloud: {
-        clientId: '2fc45972-0302-4a7a-b6b1-e83784579922',
-        clientSecret: 'wYEgcojWmk_tXPEq2l-9THSDLOdRGIW3ug9LLkvBz5A'
+        clientId: '',
+        clientSecret: ''
     }
 }
